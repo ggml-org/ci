fc26ab99783c3943318a2f067856a49efe82ac80
Georgi Gerganov <ggerganov@gmail.com>
2023-07-15 17:55:42 +0300

    env : change bot email

--- env.sh
+++ env.sh
@@ -35,7 +35,7 @@ gg_export GG_WORK_PATH     $(realpath ~/work)
 gg_export GG_WORK_BRANCHES ${GG_WORK_PATH}/branches
 
 gg_export GG_BOT_NAME  "ggml-bot"
-gg_export GG_BOT_EMAIL "ggerganov@gmail.com"
+gg_export GG_BOT_EMAIL "keytapchallenge@gmail.com"
 
 # here we will store all results
 gg_export GG_RESULTS_PATH   $(realpath ~/results)
