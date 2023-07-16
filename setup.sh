c53ee6fb14aae24cb982bab72b83175f20847fcc
Georgi Gerganov <ggerganov@gmail.com>
2023-07-16 18:28:56 +0300

    setup : remove old stuff

--- setup.sh
+++ setup.sh
@@ -30,10 +30,6 @@ function gg_setup_ggml {
     if [ ! -d $GG_GGML_DIR ]; then
         git clone $GG_GGML_REPO $GG_GGML_DIR
     fi
-
-    cd $GG_GGML_DIR
-
-    #bash ci/setup.sh
 }
 
 function gg_setup_whisper_cpp {
@@ -42,10 +38,6 @@ function gg_setup_whisper_cpp {
     if [ ! -d $GG_WHISPER_CPP_DIR ]; then
         git clone $GG_WHISPER_CPP_REPO $GG_WHISPER_CPP_DIR
     fi
-
-    cd $GG_WHISPER_CPP_DIR
-
-    #bash ci/setup.sh
 }
 
 function gg_setup_llama_cpp {
@@ -54,15 +46,6 @@ function gg_setup_llama_cpp {
     if [ ! -d $GG_LLAMA_CPP_DIR ]; then
         git clone $GG_LLAMA_CPP_REPO $GG_LLAMA_CPP_DIR
     fi
-
-    cd $GG_LLAMA_CPP_DIR
-
-    #bash ci/setup.sh
-
-    #gg_wget models/open-llama-3b ${GG_LLAMA_CPP_OPEN_LLAMA_REPO}/resolve/main/pytorch_model.bin
-    #gg_wget models/open-llama-3b ${GG_LLAMA_CPP_OPEN_LLAMA_REPO}/resolve/main/config.json
-    #gg_wget models/open-llama-3b ${GG_LLAMA_CPP_OPEN_LLAMA_REPO}/resolve/main/tokenizer.model
-    #gg_wget models/open-llama-3b ${GG_LLAMA_CPP_OPEN_LLAMA_REPO}/resolve/main/tokenizer_config.json
 }
 
 ## main
