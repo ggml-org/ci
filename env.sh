b85ce6ab463402cf4500bf46d1a140b71ae336cf
Georgi Gerganov <ggerganov@gmail.com>
2023-07-17 16:13:00 +0300

    env : fix commit status update

--- env.sh
+++ env.sh
@@ -72,7 +72,7 @@ function gg_set_commit_status {
         -H "Authorization: Bearer ${GG_SECRET_TOKEN_GH_API}"\
         -H "X-GitHub-Api-Version: 2022-11-28" \
         https://api.github.com/repos/${owner}/${repo}/statuses/${commit} \
-        -d '{"state":"'"${status}"'","target_url":"'${GG_RESULTS_REPO}'/tree/'${GG_RESULTS_BRANCH}'/'${repo}'/'${commit_path}'/'${GG_NODE}'","description":"'${desc}'","context":"ggml-org / '"${GG_NODE}"'"}'
+        -d '{"state":"'"${status}"'","target_url":"'${GG_RESULTS_REPO}'/tree/'${GG_RESULTS_BRANCH}'/'${repo}'/'${commit_path}'/'${GG_NODE}'","description":"'"${desc}"'","context":"ggml-org / '"${GG_NODE}"'"}'
 }
 
 ## general env
