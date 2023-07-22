10eacf627f9c9506a86e3d29831553ab841db75f
Georgi Gerganov <ggerganov@gmail.com>
2023-07-22 10:58:52 +0300

    install : CUDA compiler + cuBLAS

--- install-cuda.sh
+++ install-cuda.sh
@@ -4,3 +4,5 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
+sudo apt-get -y install cuda-nvcc-12-2
+sudo apt-get -y install libcublas-dev-12-2
