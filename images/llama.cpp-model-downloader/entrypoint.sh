19d7d85d39f7ec8d1d2fba206438042fe0955ddb
Pierrick HYMBERT <pierrick.hymbert@gmail.com>
2024-03-25 07:48:41 +0100

    ci: model downloader

---
+++ images/llama.cpp-model-downloader/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set +eux
+
+if [ -z "$HF_REPO" ] || [ -z "$HF_FILE" ]
+then
+  # shellcheck disable=SC2145
+  echo "invalid command: $@"
+  echo "usage: "
+  echo "export HF_REPO=ggml-org/models"
+  echo "export HF_FILE=phi-2/ggml-model-q4_0.gguf"
+  echo "$0"
+  exit 1
+fi
+
+nvidia-smi || exit 1
+
+echo "HF_REPO ${HF_REPO}"
+echo "HF_FILE ${HF_FILE}"
+MODEL_DIR=$(dirname "${HF_FILE}")
+mkdir -p "/models/$MODEL_DIR"
+
+./build/bin/main --hf-repo "${HF_REPO}" --hf-file "${HF_FILE}" --model "/models/$HF_FILE" --random-prompt --n-predict 1
