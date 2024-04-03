6b6ddb4d874ef5be82e1903e96247a5ea0c05fc3
Pierrick HYMBERT <pierrick.hymbert@gmail.com>
2024-04-03 19:46:15 +0200

    ci: start-github-runner-manager.sh do not stop at first model

--- start-github-runner-manager.sh
+++ start-github-runner-manager.sh
@@ -61,7 +61,7 @@ if [ -z "$DOWNLOAD_MODELS" ] || [ "$DOWNLOAD_MODELS" == "ON" ] ; then
             -v $MODEL_FOLDERS:/models:rw \
             -e HF_REPO="$HF_REPO" \
             -e HF_FILE="$HF_FILE" \
-            llama.cpp-model-downloader > download_model."$(basename "$HF_FILE")".log 2>&1 || echo "Error in model downloader: " && cat download_model."$(basename "$HF_FILE")".log && exit 1
+            llama.cpp-model-downloader > download_model."$(basename "$HF_FILE")".log 2>&1
     done
   ) || exit 1
 fi
